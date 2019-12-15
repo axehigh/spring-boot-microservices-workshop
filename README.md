330c7f6edc46448ad4206f9ef210c6bb5ee0911e
Oddbjørn Lona <pt184@skuld.com>
2019-12-15 11:03:49 +0100

    Working microservice in docker container

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # spring-boot-microservices-workshop
 
 Based on koushikkothagal/spring-boot-microservices-workshop
+
+Change host.ip to your local machine for all 4 projects.  
+
+`mvn clean package`
+`docker-compose up -d`
+
+Movie Catalog:  
+Goto `http://192.168.86.112:8081/catalog/1`
+
--- discovery-server/src/main/java/io/javabrains/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/io/javabrains/discoveryserver/DiscoveryServerApplication.java
@@ -1,8 +1,10 @@
 package io.javabrains.discoveryserver;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaServer
